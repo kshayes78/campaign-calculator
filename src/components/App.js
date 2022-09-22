2f649f9293172c50320d87c1bc9ea00796d69507
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-09-22 17:14:59 -0400

    working on Projected Turnout

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import '../App.css';
+import CalculateIcon from '@mui/icons-material/Calculate';
 import TurnoutCalc from './TurnoutCalc';
 
 function App() {
   return (
     <div className='App'>
+      <CalculateIcon />
       <h1>Campaign Calculators</h1>
       <TurnoutCalc />
     </div>
--- src/components/TurnoutCalc.js
+++ src/components/TurnoutCalc.js
@@ -1,84 +1,208 @@
-import React, { useState } from 'react';
+import React, { useState, setState } from 'react';
 
-function TurnoutCalc() {
-  const [midtermTurnout1, setMidTermTurnout1] = useState(0);
-  const [midtermTurnout2, setMidTermTurnout2] = useState(0);
-  const [midtermTurnout3, setMidTermTurnout3] = useState(0);
-  const [averageTurnout, setAverageTurnout] = useState(0);
-  const [totalVoters1, setTotalVoters1] = useState(0);
-  const [totalVoters2, setTotalVoters2] = useState(0);
-  const [totalVoters3, setTotalVoters3] = useState(0);
-
-  function handleTurnout(e) {
-    setMidTermTurnout1(e.target.value);
+class TurnoutCalc extends React.Component {
+  // // Midterm Voter Turnout Count (integer)
+  // const [midtermTurnout1, setMidTermTurnout1] = useState(0);
+  // const [midtermTurnout2, setMidTermTurnout2] = useState(0);
+  // const [midtermTurnout3, setMidTermTurnout3] = useState(0);
+  // //Midterm Total registered Voters
+  // const [totalVoters1, setTotalVoters1] = useState(0);
+  // const [totalVoters2, setTotalVoters2] = useState(0);
+  // const [totalVoters3, setTotalVoters3] = useState(0);
+  // //Midterm Voter Turnout Percentages
+  // const [voterTurnoutPercent1, setVoterTurnoutPercent1] = useState(0);
+  // const [voterTurnoutPercent2, setVoterTurnoutPercent2] = useState(0);
+  // const [voterTurnoutPercent3, setVoterTurnoutPercent3] = useState(0);
+  // //Midterm Turnout Average
+  // const [averageTurnout, setAverageTurnout] = useState(0);
+  constructor() {
+    super();
+    this.state = {
+      midtermTurnout1: 0,
+      midtermTurnout2: 0,
+      midtermTurnout3: 0,
+      totalVoters1: 0,
+      totalVoters2: 0,
+      totalVoters3: 0,
+      currentRegVoters: 0,
+    };
   }
 
-  function handleTurnout2(e) {
-    setMidTermTurnout2(e.target.value);
+  handleTurnout(e) {
+    // setMidTermTurnout1(e.target.value);
+    // console.log(midtermTurnout1);
+    this.setState({ midtermTurnout1: parseInt(e.target.value) });
   }
-  function handleTurnout3(e) {
-    setMidTermTurnout3(e.target.value);
+  handleTurnout2(e) {
+    // setMidTermTurnout1(e.target.value);
+    // console.log(midtermTurnout1);
+    this.setState({ midtermTurnout2: parseInt(e.target.value) });
   }
-  //Calculate Turnout Average
-  // function turnoutAverage(e) {
-  //   e.preventDefault();
-  //   setAverageTurnout(
-  //     Math.ceil(
-  //       parseInt(midtermTurnout1) +
-  //         parseInt(midtermTurnout2) +
-  //         parseInt(midtermTurnout3)
-  //     ) / 3
-  //   );
-  // }
-  //Calculate Turnout Percentage
-  function handleTurnoutPercent() {
-    setTotalVoters1(midtermTurnout1 / totalVoters1);
-    setTotalVoters2(midtermTurnout2 / totalVoters2);
-    setTotalVoters3(midtermTurnout3 / totalVoters3);
+  handleTurnout3(e) {
+    // setMidTermTurnout1(e.target.value);
+    // console.log(midtermTurnout1);
+    this.setState({ midtermTurnout3: parseInt(e.target.value) });
+  }
+  handleVoters(e) {
+    this.setState({ totalVoters1: parseInt(e.target.value) });
+  }
+  handleVoters2(e) {
+    this.setState({ totalVoters2: parseInt(e.target.value) });
+  }
+  handleVoters3(e) {
+    this.setState({ totalVoters3: parseInt(e.target.value) });
+  }
+  handleCurrentVoters(e) {
+    this.setState({ currentRegVoters: parseInt(e.target.value) });
   }
 
-  return (
-    <div className='container'>
-      <h2>Turnout Calculator</h2>
-      <form className='turnout'>
-        <label htmlFor='turnout'>
-          Please enter Total Voter Turnout % for 2010:
-        </label>
-        <input
-          onChange={handleTurnout}
-          type='number'
-          id='turnout'
-          name='turnout'
-          required
-          minLength='1'
-        ></input>
-        <label for='turnout'>
-          Please enter Total Voter Turnout % for 2014:{' '}
-        </label>
-        <input
-          onChange={handleTurnout2}
-          type='number'
-          id='turnout'
-          name='turnout'
-          required
-          minLength='1'
-        ></input>
-        <label for='turnout'>
-          Please enter Total Voter Turnout % for 2018:{' '}
-        </label>
-        <input
-          onChange={handleTurnout3}
-          type='number'
-          id='turnout'
-          name='turnout'
-          required
-          minLength='1'
-        ></input>
-        <button onClick={turnoutAverage}>Submit</button>
-        <h2>Your average turnout is {averageTurnout}%.</h2>
-      </form>
-    </div>
-  );
+  calculatePercentage(num, den) {
+    if (!den) {
+      return 0;
+    }
+    return (num / den) * 100;
+  }
+  calculateTurnout(num1, num2) {
+    return num1 * num2;
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <h2>Turnout Calculator</h2>
+        <form className='turnout'>
+          <label htmlFor='turnout'>
+            Please enter Total Voter Turnout Count for 2010:
+          </label>
+          <input
+            onChange={this.handleTurnout.bind(this)}
+            type='number'
+            id='turnout1'
+            name='turnout1'
+            required
+            minLength='1'
+          ></input>
+          <br />
+          <label htmlFor='regVoters'>
+            Please enter Total Registered Voter for 2010:
+          </label>
+          <input
+            onChange={this.handleVoters.bind(this)}
+            type='number'
+            id='voter1'
+            name='voter1'
+            required
+            minLength='1'
+          ></input>
+          <br />
+          <label htmlFor='turnout'>
+            Please enter Total Voter Turnout Count for 2014:
+          </label>
+          <input
+            onChange={this.handleTurnout2.bind(this)}
+            type='number'
+            id='turnout2'
+            name='turnout2'
+            required
+            minLength='1'
+          ></input>
+          <br />
+          <label htmlFor='regVoters'>
+            Please enter Total Registered Voter for 2014:
+          </label>
+          <input
+            onChange={this.handleVoters2.bind(this)}
+            type='number'
+            id='voter2'
+            name='voter2'
+            required
+            minLength='1'
+          ></input>
+          <br />
+          <label htmlFor='turnout'>
+            Please enter Total Voter Turnout Count for 2018:
+          </label>
+          <input
+            onChange={this.handleTurnout3.bind(this)}
+            type='number'
+            id='turnout3'
+            name='turnout3'
+            required
+            minLength='1'
+          ></input>
+          <br />
+          <label htmlFor='regVoters'>
+            Please enter Total Registered Voter for 2018:
+          </label>
+          <input
+            onChange={this.handleVoters3.bind(this)}
+            type='number'
+            id='voter3'
+            name='voter3'
+            required
+            minLength='1'
+          ></input>
+          <br />
+          <label htmlFor='regVoters'>
+            Please enter Total Registered Voter for the Current Year:
+          </label>
+          <input
+            onChange={this.handleCurrentVoters.bind(this)}
+            type='number'
+            id='currentVoters'
+            name='currentVoters'
+            required
+            minLength='1'
+          ></input>
+
+          <h2>
+            Your average turnout
+            <br />
+            For 2010 is{' '}
+            {this.calculatePercentage(
+              this.state.midtermTurnout1,
+              this.state.totalVoters1
+            )}
+            %. <br />
+            For 2014 is{' '}
+            {this.calculatePercentage(
+              this.state.midtermTurnout2,
+              this.state.totalVoters2
+            )}
+            %. <br />
+            For 2018 is{' '}
+            {this.calculatePercentage(
+              this.state.midtermTurnout3,
+              this.state.totalVoters3
+            )}
+            %.
+            <br /> Your average turnout is{' '}
+            {this.calculatePercentage(
+              this.state.midtermTurnout1 +
+                this.state.midtermTurnout2 +
+                this.state.midtermTurnout3,
+
+              this.state.totalVoters1 +
+                this.state.totalVoters2 +
+                this.state.totalVoters3
+            )}
+            %.
+            <br />
+            {/* Your Projected Voter Turnout is{' '}
+            {this.calculateTurnout(
+              this.state.currentRegVoters,
+              (this.state.midtermTurnout1 +
+                this.state.midtermTurnout2 +
+                this.state.midtermTurnout3,
+              this.state.totalVoters1 +
+                this.state.totalVoters2 +
+                this.state.totalVoters3) * 100
+            )} */}
+          </h2>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TurnoutCalc;
